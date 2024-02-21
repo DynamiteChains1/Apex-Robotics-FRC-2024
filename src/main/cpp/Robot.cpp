c951d327b28485f193b20ea420a8b371707ab14a
DynamiteChains1 <jfp6724@students.mpsaz.org>
2024-02-21 14:29:35 -0700

    Added the code for camera output that sets up for april tag processing and made the code easier to read

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,19 +2,27 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//Includes for VisionProcessing
+#include <cstdio>
+#include <thread>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <cameraserver/CameraServer.h>
+#include <opencv2/opencv.hpp>
+
+//Main includes neccessary for robot functionality
 #include <frc/TimedRobot.h>
 #include <frc/Timer.h>
 #include <frc/Joystick.h>
 #include <frc/drive/DifferentialDrive.h>
-#include <frc/Filesystem.h>
-#include <frc/controller/PIDController.h>
-#include <frc/controller/SimpleMotorFeedforward.h>
+
+//Includes for using shooter/intake motors
 #include <rev/CANSparkMax.h>
+
+//Includes for playing music on Falcon500s
 #include <ctre/phoenix6/Orchestra.hpp>
 #include <ctre/phoenix6/TalonFX.hpp>
-#include <string>
-#include <cstring>
 
+//Robot class for all of our functions and controlling the robot
 class Robot : public frc::TimedRobot {
  public:
   Robot() {
@@ -27,19 +35,24 @@ class Robot : public frc::TimedRobot {
     m_right.SetInverted(true);
     m_robotDrive.SetExpiration(100_ms);
     m_timer.Start();
+
+    //Shooter inversion/follow setup
     s_follow.SetInverted(true);
     c_follow.SetInverted(true);
     s_follow.Follow(s_lead);
     c_follow.Follow(c_lead);
-    //Old code that overcomplicates something that already happens
-    /*const char *dp = deploy_path.c_str(); */
+
+    //Load music and setup orchestra
     m_orchestra.LoadMusic("sus.chrp");
     m_orchestra.AddInstrument(m_sus1);
     m_orchestra.AddInstrument(m_sus2);
+
+    //Shooter PID setup using Shooter PID variables
     sPID.SetP(kShootP);
     sPID.SetI(kShootI);
     sPID.SetD(kShootD);
     sPID.SetFF(kShootFF);
+    //This is to make sure its not trying to output more than possible
     sPID.SetOutputRange(-1.0, 1.0);
   }
 
@@ -70,8 +83,9 @@ class Robot : public frc::TimedRobot {
   void TeleopInit() {m_orchestra.LoadMusic("sus.chrp");}
 
   void TeleopPeriodic() override {
-    // Drive with arcade style (use right stick to steer)
+    // Drive with arcade style
     m_robotDrive.ArcadeDrive(-m_stick.GetY(), m_stick.GetTwist());
+
     // Code for shooter
     if (m_stick.GetRawButton(1)) {
       s_setpoint = 300;
@@ -80,15 +94,18 @@ class Robot : public frc::TimedRobot {
       s_setpoint = 0;
     }
     sPID.SetReference(s_setpoint, rev::CANSparkMax::ControlType::kVelocity);
-    //Code for playing among us music
+
+    //Code for playing/pausing among us music
     if (m_stick.GetRawButton(6)) {
       m_orchestra.Play();
     }
     if (m_stick.GetRawButton(5)) {
       m_orchestra.Pause();
     }
+
     //This function tests for controller input to increase/decrease the speed of the climber
     ClimberSpeed();
+
     //This tests for controller input to climb up/down
     if (m_stick.GetRawButton(4)) {
       c_lead.Set(c_speed);
@@ -124,27 +141,28 @@ class Robot : public frc::TimedRobot {
   ctre::phoenix6::hardware::TalonFX m_sus1{3};
   ctre::phoenix6::hardware::TalonFX m_sus2{4};
   frc::DifferentialDrive m_robotDrive{m_left, m_right};
+
   // Setup motors for intake
   rev::CANSparkMax i_turn{5, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax i_take{6, rev::CANSparkMax::MotorType::kBrushless};
+
   // Setup motors for shooter
   rev::CANSparkMax s_lead{7, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax s_follow{8, rev::CANSparkMax::MotorType::kBrushless}; 
+
   // Setup motors for climber
   rev::CANSparkMax c_lead{10, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax c_follow{9, rev::CANSparkMax::MotorType::kBrushless};
+
   // Setup Joystick and Timer
   frc::Joystick m_stick{0};
   frc::Timer m_timer;
-
-  // Gets deploy directory and sets it to a callable 
-  //This code is uneccessary and overcomplicates stuff that already happens
-  /*::string deploy_path = frc::filesystem::GetDeployDirectory();*/
   
   // Setup Orchestra
   ctre::phoenix6::Orchestra m_orchestra;
   
   bool sus_time = false;
+
   // PID setup for shooter
   double s_setpoint = 0;
   double kShootFF = 0.000015;
@@ -152,6 +170,7 @@ class Robot : public frc::TimedRobot {
   double kShootI = 0.000001;
   double kShootD = 0.0;
   rev::SparkPIDController sPID = s_lead.GetPIDController();
+
   // PID setup for intake rotation
   double i_setpos = 0;
   double kI_turnFF = 0;
@@ -159,8 +178,11 @@ class Robot : public frc::TimedRobot {
   double kI_turnI = 0;
   double kI_turnD = 0;
   rev::SparkPIDController i_turnPID = i_turn.GetPIDController();
+
   // Variable setup for climber
   double c_speed = 1;
+
+  //Climber speed change function
   void ClimberSpeed() {
     if (m_stick.GetRawButtonPressed(10)){
       if (c_speed < 1) {
@@ -173,6 +195,39 @@ class Robot : public frc::TimedRobot {
       }
     }
   }
+  //April Tag stuff + camera
+  static void VisionThread() {
+    //Get usb input from cameraserver
+    cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
+    camera.SetResolution(640, 480);
+
+    //Make a CV sink to hold Mats (images/frames but for OpenCV)
+    cs::CvSink cvSink = frc::CameraServer::GetVideo();
+
+    //Direct the output back to the dashboard with CVSource
+    cs::CvSource camOutStream = frc::CameraServer::PutVideo("CameraOutput", 640, 480);
+
+    //Never explained why, but we reuse this mat bc mats
+    //are memory extensive. Don't know how it is being reused
+    //or when it started existing in the first place
+    cv::Mat mat;
+
+    //Image to mat conversion loop
+    while (true) {
+
+      //Tells the CvSink to grab a frame and put it into the mat
+      //and notify of errors
+      if (cvSink.GrabFrame(mat) == 0) {
+        //Sends Camera Output Stream the error
+        camOutStream.NotifyError(cvSink.GetError());
+        //skip rest of loop because of error
+        continue;
+      }
+      //Give camera stream new image to display
+      camOutStream.PutFrame(mat);
+    }
+  }
+
 };
 
 
